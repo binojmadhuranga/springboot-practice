fe91e452a6904f30c8aa307c7d794edbad4b3d68
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-06-24 19:25:47 +0530

    first commit

---
+++ demo.afsd/src/main/java/lk/acpt/demo/afsd/entity/Customer.java
@@ -0,0 +1,25 @@
+package lk.acpt.demo.afsd.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private  double salary;
+
+    private String email;
+
+
+
+}
